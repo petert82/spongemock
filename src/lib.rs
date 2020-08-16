2463b424d3a0569e48d94e8af0ad8e642832f9a2
Peter Thompson <peter.thompson@dunelm.org.uk>
2020-08-16 20:51:11 +0200

    Fix fmt

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub fn mock<S: Into<String>>(input: S) -> String {
     let mut make_uppercase = true;
 
     for c in input.chars() {
-        if make_uppercase && c.is_lowercase(){
+        if make_uppercase && c.is_lowercase() {
             for upper in c.to_uppercase() {
                 out.push(upper);
             }
@@ -42,10 +42,19 @@ mod test {
 
     #[test]
     fn basic_mocking() {
-        assert_eq!(mock("abcdefghijklmnopqrstuvwxyz"), "AbCdEfGhIjKlMnOpQrStUvWxYz".to_string());
-        assert_eq!(mock("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "AbCdEfGhIjKlMnOpQrStUvWxYz".to_string());
+        assert_eq!(
+            mock("abcdefghijklmnopqrstuvwxyz"),
+            "AbCdEfGhIjKlMnOpQrStUvWxYz".to_string()
+        );
+        assert_eq!(
+            mock("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+            "AbCdEfGhIjKlMnOpQrStUvWxYz".to_string()
+        );
         assert_eq!(mock("a1b2c3 d4e5f6"), "A1b2C3 d4E5f6".to_string());
-        assert_eq!(mock("1234567890!@#$%^&*()-=_+"), "1234567890!@#$%^&*()-=_+".to_string());
+        assert_eq!(
+            mock("1234567890!@#$%^&*()-=_+"),
+            "1234567890!@#$%^&*()-=_+".to_string()
+        );
         assert_eq!(mock("äöüßßâç"), "ÄöÜßSSâÇ".to_string());
         assert_eq!(mock("✌🤞😎"), "✌🤞😎".to_string());
     }
